02aeb4fab22196a69feeb676b0f177a7237fa354
Warda Jibril <Jibril.Warda@gmail.com>
2024-02-13 11:30:13 +0000

    commit with debugged code

--- milestone_2.py
+++ milestone_2.py
@@ -6,9 +6,9 @@ print(word_list)
 word = random.choice(word_list)
 print(word)
 
-guess = len(input("Enter a letter:"))
-
-if guess == 1 and guess.isalpha():
+guess = str(input("Enter a letter:"))
+length_guess = len(guess)
+if length_guess == 1 and guess.isalpha():
     print("Good guess!")
 else:
     print("Oops! That is not a valid input")
