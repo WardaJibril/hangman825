092a8d17b1bed04f3cf5576312037f82dcea68f4
Warda Jibril <Jibril.Warda@gmail.com>
2024-02-13 11:41:12 +0000

    commits functions to ask for user input and check the users guess

---
+++ milestone_3.py
@@ -0,0 +1,19 @@
+def check_guess(guess):
+
+    guess = guess.lower()
+
+    if guess in word:
+        print(f"Good guess!{guess}is in the word.")
+    else:
+        print (f"Sorry, {guess} is not in the word. Try again.")
+
+def ask_user_input(guess):
+    while True:
+      guess = input("Guess a letter:")
+      length_guess = len(guess)
+      if guess.isnotaplpha() and length_guess != 1:
+        print("Invalid letter. Please, enter a single alphabetical character.")
+      else:
+        break
+    check_guess(guess)
+    
