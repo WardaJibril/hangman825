a4ce3af347f8c23605c900187ecd45cc760fb078
Warda Jibril <Jibril.Warda@gmail.com>
2024-02-13 11:07:52 +0000

    commits changes to spacing in lines of code for ease of reading

--- milestone_2.py
+++ milestone_2.py
@@ -1,8 +1,11 @@
 import random
+
 word_list = ["kiwi", "raspberry","orange","mango", "pineapple"]
 print(word_list)
+
 word = random.choice(word_list)
 print(word)
+
 guess = len(input("Enter a letter:"))
 
 if guess == 1 and guess.isalpha():
