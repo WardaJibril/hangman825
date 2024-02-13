1e7f08ba774eb303f8ddedd5188914690b2db732
Warda Jibril <Jibril.Warda@gmail.com>
2024-02-13 11:05:47 +0000

    commits list of words, user input and random choice of words in list

---
+++ milestone_2.py
@@ -0,0 +1,12 @@
+import random
+word_list = ["kiwi", "raspberry","orange","mango", "pineapple"]
+print(word_list)
+word = random.choice(word_list)
+print(word)
+guess = len(input("Enter a letter:"))
+
+if guess == 1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input")
+
